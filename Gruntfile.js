c7c25a7259025df172d37eb831e0911964935d8c
Robert Winterbottom <rwinterbottom@blueraster.com>
2015-05-20 11:42:01 -0400

    updated readme and package.json to get ready for next release

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,9 +45,7 @@ module.exports = function(grunt) {
 
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
+  
   grunt.registerTask('test', ['ftp_push']);
   grunt.registerTask('sample', ['ftp_push:sample']);
 
