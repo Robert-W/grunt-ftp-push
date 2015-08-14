7d05e913a2989559d70755250fc53c4e93750f2c
Robert Winterbottom <rwinterbottom@blueraster.com>
2015-08-13 22:06:57 -0400

    started rewriting the plugin

--- tasks/messages.js
+++ tasks/messages.js
@@ -1,10 +1,12 @@
 module.exports = {
-  connectionClosed: 'FTP connection closed!',
-  anonymousLogin: 'Attempting anonymous login.',
   invalidRequirements: 'You did not provide the minimum requirements.  Please refer to the following documentation for information. https://github.com/Robert-W/grunt-ftp-push#required-options',
-  authSuccess: function (user) { return user + ' successfully authenticated!'; },
-  fileNotExist: function (path) { return 'Source file ' + path + ' not found.'; },
+  anonymousLogin: 'Attempting anonymous login.',
+  connectionClosed: 'FTP connection closed!',
   directoryCreated: function (directory) { return directory + ' directory created successfully.'; },
   fileTransferFail: function (file, err) { return file + ' failed to transfer because ' + err; },
   fileTransferSuccess: function (file) { return file + ' transferred successfully.'; },
+  fileNotExist: function (path) { return 'Source file ' + path + ' not found.'; },
+  authFailure: function (user) { return 'Could not authenticate with ' + user; },
+  authSuccess: function (user) { return user + ' successfully authenticated!'; },
+  debug: function (msg) { return 'DEBUG: ' + msg; }
 };
